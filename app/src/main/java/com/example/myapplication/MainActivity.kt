37c205fb8684f9daa8a2343561ed3866b0e3e65a
NataliaElnikova <nataliaelnikova@gmail.com>
2023-05-07 00:44:23 +0300

    Add menu buttons, Insert movie posters, Insert text fields, Apply Drawable for backgrounds, Apply CardView for rounded corners and levitation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.filament.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun initButtons() {
+
+        fun onClickToast(view: View) {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
