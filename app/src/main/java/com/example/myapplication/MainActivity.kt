63ad843bd939baccefa32f9508fb535fe3b4832e
NataliaElnikova <nataliaelnikova@gmail.com>
2023-05-08 11:55:07 +0300

    Add menu buttons,insert movie posters,insert text fields,apply Drawable for backgrounds,apply CardView for rounded corners and levitation and add toolbar menu

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,16 +9,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-    fun initButtons() {
-
-        fun onClickToast(view: View) {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-        }
     }
 }
+//
+//    fun onClickToast(view: android.view.View) {
+//
+//        btn_menu.onClickToast() {
+//            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+//        }
+//        btn_favorites.onClickToast() {
+//            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+//        }
+//        btn_watch_later.onClickToast() {
+//            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+//        }
+//        btn_selections.onClickToast() {
+//            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+//        }
+//        btn_settings.onClickToast() {
+//            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//}
