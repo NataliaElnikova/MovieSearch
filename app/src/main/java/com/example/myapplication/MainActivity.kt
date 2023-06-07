23f32acec7a688b9c0e5cc6d729f0e7b462f2f09
NataliaElnikova <nataliaelnikova@gmail.com>
2023-06-07 11:21:19 +0300

    Refactor view items on a material components add a toolbar in activity_main.xml move menu buttons to toolbar and navigation bar couldn't implement a dark theme...

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,31 +3,59 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.filament.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.myapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
+
+        initNavigation()
+        bottomNavigation()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun initNavigation() {
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun bottomNavigation() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
 }
-//
-//    fun onClickToast(view: android.view.View) {
-//
-//        btn_menu.onClickToast() {
-//            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
-//        }
-//        btn_favorites.onClickToast() {
-//            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-//        }
-//        btn_watch_later.onClickToast() {
-//            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-//        }
-//        btn_selections.onClickToast() {
-//            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-//        }
-//        btn_settings.onClickToast() {
-//            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-//        }
-//    }
-//}
